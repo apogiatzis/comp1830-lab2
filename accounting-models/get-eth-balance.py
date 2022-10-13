f8a0934cd6ca914eed60af5b71a3c1ffd5f50492
apogiatzis <apoyiatzis@outlook.com>
2022-10-13 12:10:52 +0100

    update rpc node

--- accounting-models/get-eth-balance.py
+++ accounting-models/get-eth-balance.py
@@ -1,9 +1,9 @@
 from web3 import Web3
 
-rpc_node='https://speedy-nodes-nyc.moralis.io/46fb5bf54b9f7ee7eb4b0a54/eth/mainnet' 
+rpc_node='https://mainnet.infura.io/v3/340ab19d3ab14fcea1d94fb2adde170b' 
 w3 = Web3(Web3.HTTPProvider(rpc_node))
 
-account = "<address here>"
+account = "0xEeC84548aAd50A465963bB501e39160c58366692"
 
 def get_balance(address):
     return w3.eth.getBalance(w3.toChecksumAddress(address))
